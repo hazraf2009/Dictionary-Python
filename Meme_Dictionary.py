8c13e219e938853abea79eafe868f991f965e88c
hazraf2009 <73149382+hazraf2009@users.noreply.github.com>
2024-05-19 17:22:10 +0700

    Create Meme_Dictionary.py

---
+++ Meme_Dictionary.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "SHEES" : "sedikit tidak kesetujuan",
+            "CREEPY": "menakutkan, tidak menyenangkan",
+            "AGGRO" : "untuk menjadi agresif/marah"
+            }
+            
+for i in range(5);
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    word = word.upper()
+    
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("tidak ada di dict")
